41b81f274ddc394b3c876d30ebd875eff305cb4f
Sebastian Ahumada <cbasmx@gmail.com>
2022-03-21 18:48:38 -0300

    control props alt

--- src/02-component-patterns/hooks/useProduct.ts
+++ src/02-component-patterns/hooks/useProduct.ts
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect,  useState } from "react";
 import { onChangeArgs, Product } from '../interfaces/interfaces';
 
 
@@ -13,16 +13,10 @@ export const useProduct = ({onChange, product, value=0}: useProductArgs)=> {
 
     const [counter, setCounter] = useState<number>(value);
 
-    const isControlled = useRef(!!onChange);
+    
 
     const increaseBy = (value:number) => {
-        // console.log('isControlled',isControlled.current);
-
-        if(isControlled.current){
-            return onChange!({count: value, product});
-        }
-
-
+        // console.log('isControlled',isControlled.current);        
 
         const newValue = Math.max(counter + value, 0)
         setCounter(newValue);
